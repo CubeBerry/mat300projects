559a40095d043bd1ffd697a54288b70c3290d393
rapt0r <minjae.yoo@digipen.edu>
2025-01-08 19:29:35 -0800

    Fixed Directories Path

--- MAT300Projects/main.cpp
+++ MAT300Projects/main.cpp
@@ -2,9 +2,61 @@
 #include <glew.h>
 #include <iostream>
 
-#undef main
+int main(int argc, char* argv[]) {
+    // SDL 초기화
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
+        return -1;
+    }
 
-int main(int argc, char* argv[])
-{
-	return 0;
+    // OpenGL 3.3 Core Profile 설정
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    // SDL 윈도우 생성
+    SDL_Window* window = SDL_CreateWindow(
+        "GLEW + SDL2 Test",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        800, 600,
+        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
+    );
+
+    if (!window) {
+        std::cerr << "Failed to create SDL window: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return -1;
+    }
+
+    // OpenGL 컨텍스트 생성
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+    if (!glContext) {
+        std::cerr << "Failed to create OpenGL context: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    // GLEW 초기화
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        std::cerr << "GLEW initialization failed: " << glewGetErrorString(err) << std::endl;
+        SDL_GL_DeleteContext(glContext);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    // OpenGL 및 GLEW 버전 출력
+    std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GLEW Version: " << glewGetString(GLEW_VERSION) << std::endl;
+
+    // 화면을 3초 동안 유지
+    SDL_Delay(3000);
+
+    // 리소스 해제
+    SDL_GL_DeleteContext(glContext);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
